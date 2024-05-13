68c78adf6cb52447b7717e81eeb6dec1d18e4de6
HannaKarlson <hannaevabirgittak@gmail.com>
2024-05-13 12:46:27 +0200

    added env variables

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+import {API_KEY, BASE_URL} from 'react-native-dotenv'
+
+const App = () => {
+  return(
+<View>
+  <Text>My movies</Text>
+</View>
+  )
+}
+
+export default App
